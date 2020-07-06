6cc6f63ffa70cf104cb1b3053f1fc0f1c8e757b9
silvamatheus <msilva@saojose.br>
2020-07-05 22:31:41 -0300

    primeiro desafio feito

---
+++ code-micro-videos/app/Models/Genre.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Traits\Uuid;
+
+class Genre extends Model
+{
+    use SoftDeletes, Uuid;
+
+    protected $fillable = ['name', 'is_active'];
+    protected $dates = ['deleted_at'];
+    protected $casts = [
+    	'id' => 'string'
+    ];
+}
